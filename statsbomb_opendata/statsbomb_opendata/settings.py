08374764ae02a42975f0dc284140d5abdac35835
Jules Girard <jules.girard@outlook.com>
2023-01-14 16:14:37 +0100

    Update settings.

--- statsbomb_opendata/statsbomb_opendata/settings.py
+++ statsbomb_opendata/statsbomb_opendata/settings.py
@@ -72,6 +72,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'statsbomb_opendata.wsgi.application'
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [],
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
